e307918101642231b390407dd935d9e98b8fd7fb
HeszaR <heszar@gmail.com>
2016-03-06 13:54:29 +0100

    Done

--- src/Login.java
+++ src/Login.java
@@ -2,11 +2,7 @@
  * Created by Hesam Zarza - 0775768.
  */
 
-import com.sun.org.apache.xpath.internal.SourceTree;
-import sun.plugin2.message.Message;
-
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
@@ -20,14 +16,26 @@ public class Login {
 
     public static void main(String[] Args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 
+        Scanner sc = new Scanner("files/login.txt");
+        String line = sc.next();
 
-        Scanner scan = new Scanner (new File(files/login.txt));
-        System.out.println(scan);
-        //String user = scan.nextLine();
-        //String pass = scan.nextLine(); // looks at selected file in scan
+        System.out.println(line);
 
-        BufferedReader testingDoc = getFileReader("files/login.txt");
+        sc.close();
+        /*
+        try {
+            Scanner input;
+            File file = new File("files/login.txt");
 
+            input = new Scanner(file);
+            while (input.hasNextLine()) {
+                String line = input.nextLine();
+            }
+            input.close();
+        } catch(Exception ex){
+            ex.printStackTrace();
+        }
+*/
 
 
         Scanner keyboard = new Scanner (System.in);
@@ -35,21 +43,28 @@ public class Login {
         String inpPass = keyboard.nextLine(); // gets input from user
 
         MessageDigest md = MessageDigest.getInstance("SHA-256");
-        String text = inpUser+inpPass;
-        md.update( text.getBytes("UTF-8") );
+        String hashed = inpUser+inpPass;
+        md.update( hashed.getBytes("UTF-8") );
 
         byte[] digest = md.digest();
 
         String s = String.format("%064x", new java.math.BigInteger(1, digest));
+
         System.out.println(s);
 
-        System.out.println(text);
+        Users hesam = new Users("hesam", "ff10b229a15fef22e0dbde72e3bb09e0122926959c06b2c5b6548e437c712dc5");
+        Users walter = new Users("walter", "8cee24cc4b77db832c34abb40c41d1cf71566a1fdc20e590733e3f1ab5beb5ed");
 
-       /*if (inpUser.equals(s) && inpPass.equals()) {
-            System.out.print("your login message");
-       } else {
-           System.out.print("your error message");
-       }*/
+        String master = "555";
+
+
+         if ((hesam.getName().equals(inpUser) && hesam.getPass().equals(s)) || (inpUser.equals(hesam.getName()) && inpPass.equals(master))){
+            System.out.print("Success full login yeeeeey!");
+       }
+
+     else {
+         System.out.print("your error message");
+     }
 
     }
 
@@ -65,3 +80,20 @@ public class Login {
 
 
 }
+
+/*
+
+        Scanner scan = new Scanner(file);
+        //String user = scan.nextLine();
+        //String pass = scan.nextLine(); // looks at selected file in scan
+        File file = new File("files/login.txt");
+        System.out.println(file.exists());
+
+        BufferedReader testingDoc = getFileReader("files/login.txt");
+        System.out.println(testingDoc);
+
+        String master = "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5";
+       if (inpUser.equals(inpUser) && inpPass.equals(inpPass)) {
+            System.out.print("your login message");
+       }
+*/
---
+++ src/Users.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Hesam Zarza - 0775768.
+ */
+public class Users {
+
+    private String name;
+    private String pass;
+
+    public Users(String n, String p){
+        this.name = n;
+        this.pass = p;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+}
